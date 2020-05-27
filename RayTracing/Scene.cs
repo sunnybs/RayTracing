a9f10b2c5eb165ca26f4ad6ccde12d1fa22b6b8c
sunnybs <anokhrin8@gmail.com>
2020-05-28 01:26:41 +0500

    some changes

--- RayTracing/Scene.cs
+++ RayTracing/Scene.cs
@@ -11,12 +11,12 @@ namespace RayTracing
         public static Vector3 CameraPosition = new Vector3(0,0,0);
         public static float ViewportSize = 1.0f;
         public static float ZProjection = 1.0f;
-        public static Color BackgroundColor = Color.DarkCyan;
+        public static Color BackgroundColor = Color.LightSkyBlue;
 
         public static IReadOnlyList<Sphere> Objects = new List<Sphere>
         {
-            new Sphere(new Vector3(-1, -8, 25), 0.3f, Color.CornflowerBlue, 1000, 0.3f),
-            new Sphere(new Vector3(1, -8, 25), 0.3f, Color.CornflowerBlue, 1000, 0.3f),
+            new Sphere(new Vector3(-1, -8, 25), 0.3f, Color.CornflowerBlue, 1000, 0.5f),
+            new Sphere(new Vector3(1, -8, 25), 0.3f, Color.CornflowerBlue, 1000, 0.5f),
 
             new Sphere(new Vector3(0, -7, 25), 0.3f, Color.DarkGoldenrod, 1000, 0.3f),
 
@@ -26,8 +26,10 @@ namespace RayTracing
 
             new Sphere(new Vector3(0, -10, 30), 3f, Color.Aquamarine, 10000, 0.1f),
             new Sphere(new Vector3(0, -8, 30), 4f, Color.White, 1000, 0.1f),
-            new Sphere(new Vector3(0, 0, 30), 5f, Color.White, 100, 0.2f),
-            new Sphere(new Vector3(0, 10, 30), 6f, Color.White, 10, 0.3f),
+            new Sphere(new Vector3(0, 0, 30), 5f, Color.White, 100, 0.1f),
+            new Sphere(new Vector3(0, 10, 30), 6f, Color.White, 10, 0.1f),
+
+            new Sphere(new Vector3(0, 105, 30), 90f, Color.WhiteSmoke, 10, 0.1f),
         };
 
         public static Light AmbientLight = new Light(LightType.Ambient, 0.2f);
